086c4c85a841350f5504cce6c6fd2bb879536bb1
seraina-chiara <seraina.meister@students.fhnw.ch>
2025-09-22 11:39:11 +0200

    aufgabe 9 done

--- Chapter_02/Chapter_02_Solutions/9_solutions.py
+++ Chapter_02/Chapter_02_Solutions/9_solutions.py
@@ -2,11 +2,12 @@ distance = float(input("Bitte geben Sie die gefahrene Strecke in km an: "))
 usage = float(input("Bitte geben Sie den Verbrauch auf 100km an: "))
 price_per_liter = float(input("Bitte geben Sie den Benzinpreis pro Liter an: "))
 
-liter = distance / usage
-price = liter * price_per_liter
+liter = (distance / 100) * usage
+price = float(liter * price_per_liter)
 
 
-print(f'{"Gefahrene Strecke:":<15} {distance:^10} km')
-print(f'{"Verbrauch:":<15} {usage} Liter/100km')
-print(f'{"Benzinpreis:":<15} {price_per_liter} EUR/Liter')
-print(f'{"Benötigte Menge:":<15} {liter} km')
+print(f'{"Gefahrene Strecke:":<25} {distance:^10} km')
+print(f'{"Verbrauch:":<25} {usage} Liter/100km')
+print(f'{"Benzinpreis:":<25} {price_per_liter} EUR/Liter')
+print(f'{"Benötigte Menge:":<25} {liter} Liter')
+print(f'{"GESAMTKOSTEN:":<25} {price:.2f} EUR')
